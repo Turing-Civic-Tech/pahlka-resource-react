35ed8281098909ba73b4bc2bf99e86307c84578c
Jesse Packwood <packwoodjustin@gmail.com>
2017-12-05 12:29:04 -0700

    add ul for links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Nav from './Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         <header className="App-header">
         <h1> Resource Matching </h1>
         </header>
