cfae049095131e3dfac54249262ce0a41ecde28f
Casey Dallavalle <cbdallavalle@gmail.com>
2018-01-19 11:14:35 -0700

    Add login component and baseline styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import Nav from '../Nav/Nav.js';
-import Card from '../Card/Card.js'
+import Card from '../Card/Card.js';
+import Login from '../Login/Login.js';
+
+        // <Nav />
+        // <header className="App-header">
+        // <h1> Resource Matching </h1>
+        // </header>
+        // <Card />
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
-        <header className="App-header">
-        <h1> Resource Matching </h1>
-        </header>
-        <Card />
+        <Login />
       </div>
     );
   }
